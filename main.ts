3c79487970b489baad1e3098992e8be410e15946
J. Enrique Agudo <jeagudo@unex.es>
2021-09-23 19:49:24 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,19 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(input.temperature())
+    temperatura = 0
+    for (let index = 0; index <= num_lecturas; index++) {
+        temperatura = temperatura + input.temperature()
+        basic.pause(1000)
+    }
+    basic.showNumber(temperatura / num_lecturas)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Math.round(Math.map(input.lightLevel(), 0, 255, 0, 100)))
 })
+let temperatura = 0
+let num_lecturas = 0
+num_lecturas = 5
 basic.showString("A-Temp B-Lum")
 basic.forever(function () {
 	
