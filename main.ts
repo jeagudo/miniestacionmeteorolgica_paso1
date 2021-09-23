44b6997b8bb44606a53c3c9df1489e9674a4d4ee
J. Enrique Agudo <jeagudo@unex.es>
2021-09-23 14:13:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Math.round(Math.map(input.lightLevel(), 0, 255, 0, 100)))
+})
+basic.showString("A-Temp B-Lum")
+basic.forever(function () {
+	
+})
