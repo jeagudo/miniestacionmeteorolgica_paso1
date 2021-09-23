908778ac8e8e4aac224a16f9680e58a4c8feaff3
J. Enrique Agudo <jeagudo@unex.es>
2021-09-23 14:19:35 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.temperature())
+})
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Math.round(Math.map(input.lightLevel(), 0, 255, 0, 100)))
 })
